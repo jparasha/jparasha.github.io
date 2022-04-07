ff1d3df721969c4f3c37267e44cf62560d062ec6
jparasha <jayantparashar@outlook.com>
2022-04-07 23:05:27 +0530

    feature : react 18 upgrade

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
