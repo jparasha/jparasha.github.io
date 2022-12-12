60cdd24e6ce1b15497ea7f727f77d5c3f5ae7ca2
Jayant Parashar <jayantparashar@outlook.com>
2022-12-12 19:13:42 +0000

    refactor : exported projects to json

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,69 +1,26 @@
+import { useEffect, useState } from "react";
 import Chips from "../Chips";
-import TeamWork1 from "../../images/vaccine.svg";
-import TeamWork2 from "../../images/next.svg";
-import TeamWork3 from "../../images/cuate.svg";
-import TeamWork6 from "../../images/pana.svg";
-import TeamWork4 from "../../images/netflix.svg";
-import TeamWork5 from "../../images/weather.svg";
+// import TeamWork1 from "../../images/vaccine.svg";
+// import TeamWork2 from "../../images/next.svg";
+// import TeamWork3 from "../../images/cuate.svg";
+// import TeamWork6 from "../../images/pana.svg";
+// import TeamWork4 from "../../images/netflix.svg";
+// import TeamWork5 from "../../images/weather.svg";
 
-export default function Projects(props) {
-  const images = [
-    {
-      icon: TeamWork1,
-      title: "Vaccine Guru",
-      link: "https://vaccine-guru.vercel.app/",
-      github: "https://github.com/jparasha/vaccine-guru",
-      tech: ["React", "Next.js", "PWA"],
-      description:
-        "A web application that helps you find and book covid-19 vaccines in India for allowed age-groups.",
-    },
-    {
-      icon: TeamWork2,
-      title: "Next.js Enzyme Boilerplate",
-      link: "https://github.com/jparasha/next-scss-enzyme-boilerplate",
-      github: "https://github.com/jparasha/next-scss-enzyme-boilerplate",
-      tech: ["Next.js", "SCSS", "Jest", "Enzyme"],
-      description:
-        "A boilerplate for next.js with scss and enzyme testing. It includes a basic layout, header, footer, and a basic style guide.",
-    },
-    {
-      icon: TeamWork3,
-      title: "SpaceX Launch Tracker",
-      link: "https://spacex-missions.vercel.app/",
-      github: "https://github.com/jparasha/spacex-launches",
-      tech: ["React", "Next.js", "RWD"],
-      description:
-        "A Responsive web application that tracks SpaceX launches. User can filter results by launch year, success, or date.",
-    },
-    {
-      icon: TeamWork4,
-      title: "Netflix Clone",
-      link: "https://jparasha.github.io/yet-another-netflix-clone/",
-      github: "https://github.com/jparasha/yet-another-netflix-clone",
-      tech: ["React", "SCSS", "RestAPI"],
-      description:
-        "A web application that helps you find movies and TV shows similar to Netflix.",
-    },
-    {
-      icon: TeamWork5,
-      title: "Weather Info",
-      link: "https://jparasha.github.io/weatherinfo/",
-      github: "https://github.com/jparasha/weatherinfo",
-      tech: ["React", "Redux", "OpenWeatherMap"],
-      description:
-        "Simple Weather application that shows the weather of a city. User can search for a city and get the weather information.",
-    },
-    {
-      icon: TeamWork6,
-      inProgress: true,
-      title: "Courier Tracker",
-      link: "",
-      github: "https://github.com/jparasha/courier-dev",
-      tech: ["React", "Redux", "SCSS", "Mongo"],
-      description:
-        "Courier Tracker helps find the status of your courier. User can search for a courier and get the status of the courier.",
-    },
-  ];
+export default function Projects() {
+
+  const [projectsData, setProjectsData] = useState([]);
+  
+  useEffect(()=>{
+    fetch('/files/profile.json')
+    .then(data=>data.json())
+    .then(data=>{
+      setProjectsData(data)
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  },[]);
 
   const githubRedirect = (event, url) => {
     event.preventDefault();
@@ -80,26 +37,24 @@ export default function Projects(props) {
       </p>
       <h3 className='info-text'>Check Out Projects &#8594;</h3>
       <div className='card-row'>
-        {images.map(
+        {projectsData?.map(
           (
-            { icon, title, link, description, github, inProgress, tech },
+            { img, title, name, url, description, github, inProgress, tech },
             idx
           ) => (
             <a
-              href={link || "#"}
+              href={url || "#"}
               key={idx}
-              title={`${
-                inProgress ? title + " is under development" : "Visit " + title
-              }`}>
+              title={title}>
               <div className='card'>
                 <img
                   className='card-cover-image'
-                  src={icon}
+                  src={img}
                   alt=''
                   loading='lazy'
                 />
                 <Chips chipsData={tech} />
-                <h3 className='card-heading g-text'>{title || ""}</h3>
+                <h3 className='card-heading g-text'>{name || ""}</h3>
                 <p className='card-details'>{description || ""}</p>
                 <button
                   className='read-more'
